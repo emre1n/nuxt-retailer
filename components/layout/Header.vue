81e795eebf4ce573385156a22d21f97fefc9bf55
emre1n <emre1n@protonmail.com>
2025-02-11 15:45:25 +0300

    add login page background

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -2,7 +2,13 @@
   <header class="bg-white shadow">
     <nav class="container mx-auto px-4 py-4">
       <div class="flex justify-between items-center">
-        <NuxtLink to="/" class="text-xl font-bold">Retailer</NuxtLink>
+        <div class="flex flex-col">
+          <NuxtLink to="/" class="text-xl font-bold">Retailer</NuxtLink>
+          <span v-if="subtext" class="text-sm text-gray-600">{{
+            subtext
+          }}</span>
+        </div>
+
         <div class="flex items-center space-x-4">
           <slot name="navigation" />
           <slot name="actions" />
@@ -11,3 +17,9 @@
     </nav>
   </header>
 </template>
+
+<script setup lang="ts">
+  defineProps<{
+    subtext?: string;
+  }>();
+</script>
